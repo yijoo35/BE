0065c9d63fd30a5578bf3774f2b155247f78e7eb
zuyo7852 <leejuyeon7852@gmail.com>
2025-11-26 20:46:53 +0900

    fix: 공고 목록 조회 정렬 수정

--- src/main/java/com/plana/seniorjob/job/repository/JobPostingRepository.java
+++ src/main/java/com/plana/seniorjob/job/repository/JobPostingRepository.java
@@ -13,23 +13,28 @@ public interface JobPostingRepository extends JpaRepository<JobPosting, Long> {
     Optional<JobPosting> findByJobId(String jobId);
 
     @Query("""
-    SELECT j 
-    FROM JobPosting j
-    ORDER BY j.createDy DESC
-""")
-    List<JobPosting> findAllOrderByCreateDyDesc();
-
-    @Query("""
     SELECT j
     FROM JobPosting j
     WHERE (:keyword IS NULL
         OR j.recrtTitle LIKE %:keyword%
         OR j.oranNm LIKE %:keyword%
         OR j.jobclsNm LIKE %:keyword%
-        OR j.workPlcNm LIKE %:keyword%
         OR j.plDetAddr LIKE %:keyword%
     )
-    ORDER BY j.createDy DESC
+    ORDER BY 
+        CASE WHEN j.deadline = '마감' THEN 1 ELSE 0 END ASC,
+        j.toDd ASC,
+        j.createDy DESC
 """)
     List<JobPosting> searchJobs(@Param("keyword") String keyword);
+
+    @Query("""
+    SELECT j
+    FROM JobPosting j
+    ORDER BY 
+        CASE WHEN j.deadline = '마감' THEN 1 ELSE 0 END ASC,
+        j.toDd ASC,
+        j.createDy DESC
+""")
+    List<JobPosting> findJobs();
 }
